9fadf25fcba3d04a2d8a8be5e95ee283277f9344
ashr-aful007 <ashraful99980@gmail.com>
2022-09-03 13:32:05 +0600

    add optainal chaning and condison

--- js/news.js
+++ js/news.js
@@ -13,32 +13,29 @@ const displayCatagoris = (data) =>{
      data.forEach(news=>{
      const showNews =  document.createElement('div')
       const {title, total_view, thumbnail_url, author, details } = news;
-      // console.log(total_view)
-      // console.log(title)
-      // console.log(thumbnail_url)
-      // console.log(author)
+    
 
          console.log(news)
       showNews.innerHTML = `
       <div class="card mb-3" style="max-width: 950px;">
       <div class="row g-0">
         <div class="col-md-4 ">
-          <img src="images/news.png" class="img-fluid rounded-start w-100 h-100" alt="...">
+          <img src="${thumbnail_url}" class="img-fluid rounded-start w-100 h-100" alt="...">
         </div>
         <div class="col-md-8">
           <div class="card-body">
             <h5 class="card-title">${title}</h5>
-            <p class="card-text">${details}</p>
-            <p class="card-text"><small class="text-muted">published date: ${author.published_date}</small></p>
+            <p class="card-text">${details.slice(0,500)}</p>
+            <p class="card-text"><small class="text-muted">published date: ${author.published_date ? author.published_date : 'published date not pound' }</small></p>
             <div class="d-flex justify-content-between align-items-center">
               <div>
-                <img class="athor" src="images/news.png" alt="">
-                <p class="d-inline-block">Athor</p>
+                <img class="athor" src="${author.img ? author.img : 'img not pound' }" alt="">
+                <p class="d-inline-block fw-semibold">${author.name}</p>
               </div>
               <div>
-                <i class="fa-regular fa-eye"><span>1.5M</span></i>
+                <i class="fa-regular fa-eye">${total_view ? total_view : 'View not pound'}<span> M</span></i>
               </div>
-              <div>
+              <div d-inline-block>
                 <i class="fa-regular fa-star-half"></i>
                 <i class="fa-regular fa-star"></i>
                 <i class="fa-regular fa-star"></i>
